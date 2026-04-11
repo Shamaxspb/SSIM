52babf0b6a14449623748169c57a578b4255db61
Atharedain <shamaxrus@outlook.com>
2026-04-11 16:01:51 +0300

    Moved all nesessary assets to maintain Khaimera SKM

---
+++ Source/SSIM/Enemies/Characters/SSIMBaseEnemy.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SSIMBaseEnemy.generated.h"
+
+UCLASS(Abstract)
+class SSIM_API ASSIMBaseEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	
+};
---
+++ Source/SSIM/Enemies/Characters/SSIMKhaimeraEnemy.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SSIMKhaimeraEnemy.h"
+
+
+ASSIMKhaimeraEnemy::ASSIMKhaimeraEnemy()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ASSIMKhaimeraEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASSIMKhaimeraEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ASSIMKhaimeraEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
