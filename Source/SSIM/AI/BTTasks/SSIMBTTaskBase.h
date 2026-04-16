eb935c6ed27f7b05f6923d1a58a91b91acfde09d
Atharedain <shamaxrus@outlook.com>
2026-04-17 02:37:51 +0300

    Created Enemy Attack BTTask, added combat comp for Khaimera, started Khaimera's BTT Attack implementation

---
+++ Source/SSIM/AI/BTTasks/SSIMBTTaskBase.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "SSIMBTTaskBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SSIM_API USSIMBTTaskBase : public UBTTaskNode
+{
+	GENERATED_BODY()
+};
