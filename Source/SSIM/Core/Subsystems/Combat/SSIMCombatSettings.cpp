2cfad3a621bc78df435b0960c81eb4d2fa1f784e
Atharedain <shamaxrus@outlook.com>
2026-07-06 02:21:21 +0300

    Working on Attributes pipeline
    + Created CombatSettings, added Damage and Heal GE references there;
    + Fixed GE_Heal, GE_EnergyLevelUp;

---
+++ Source/SSIM/Core/Subsystems/Combat/SSIMCombatSettings.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SSIMCombatSettings.h"
+
+FName USSIMCombatSettings::GetCategoryName() const
+{
+	return TEXT("Game");
+}
+
+FText USSIMCombatSettings::GetSectionText() const
+{
+	return NSLOCTEXT("Settings", "Combat", "Combat Subsystem Settings");
+}
---
+++ Source/SSIM/Core/Subsystems/Combat/SSIMCombatSettings.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+
+#include "SSIMCombatSettings.generated.h"
+
+
+class UGameplayEffect;
+
+UCLASS(Config = GameplaySubsystems, DefaultConfig, meta = (DisplayName = "Progression Subsystem Settings"))
+class SSIM_API USSIMCombatSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(Config, EditAnywhere, Category = "Combat Subsystem")
+	TSubclassOf<UGameplayEffect> DamageGE;
+	
+	UPROPERTY(Config, EditAnywhere, Category = "Combat Subsystem")
+	TSubclassOf<UGameplayEffect> HealGE;
+	
+	
+	virtual FName GetCategoryName() const override;
+	virtual FText GetSectionText() const override;
+};
